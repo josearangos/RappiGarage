07595bac89819224d54b085e237b77fcc9a2944d
Juanf Gallo <jufegare@gmail.com>
2019-09-06 10:39:43 -0500

    Feat: Product detail initialization

---
+++ app/src/main/java/co/edu/udea/rappigarage_android/Product/Detail/ProductDetail.java
@@ -0,0 +1,16 @@
+package co.edu.udea.rappigarage_android.Product.Detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import co.edu.udea.rappigarage_android.R;
+
+public class ProductDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+    }
+}
