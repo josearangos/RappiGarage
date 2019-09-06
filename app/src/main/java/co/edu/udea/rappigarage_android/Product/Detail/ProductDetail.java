8876e37c580b91a7141c8c987ccbac0d9de50557
Juanf Gallo <jufegare@gmail.com>
2019-09-06 11:42:00 -0500

    Feat: Product detail view finished

--- app/src/main/java/co/edu/udea/rappigarage_android/Product/Detail/ProductDetail.java
+++ app/src/main/java/co/edu/udea/rappigarage_android/Product/Detail/ProductDetail.java
@@ -1,12 +1,14 @@
 package co.edu.udea.rappigarage_android.Product.Detail;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import android.os.Bundle;
 
 import co.edu.udea.rappigarage_android.R;
 
 public class ProductDetail extends AppCompatActivity {
+
+    private ElegantNumberButton selectedQuantity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
