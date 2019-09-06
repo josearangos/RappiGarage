fdbdd212ca2434b27e53f890bbc3cb4d7f0120fc
josearangos <jose.arangos@udea.edu.co>
2019-09-05 19:07:03 -0500

    creat login y codeconfirmation

---
+++ app/src/main/java/co/edu/udea/rappigarage_android/User/Login.java
@@ -0,0 +1,15 @@
+package co.edu.udea.rappigarage_android.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+import co.edu.udea.rappigarage_android.R;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
